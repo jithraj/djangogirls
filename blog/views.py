386ce3aa56ffbf0356e758312279a511083d30b2
G2 <jithraj97@gmail.com>
2019-07-31 12:54:18 +0530

    Learning Django

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def post_list(request):
-    return render(request,'blog/post_list.html',{})
+    me = User.objects.get(username='redhat')
+    posts = Post.objects.filter(author=me)
+    return render(request,'blog/post_list.html',{'posts': posts})
 
 
